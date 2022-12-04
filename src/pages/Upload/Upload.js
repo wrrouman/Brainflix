a181e3e82d7b2d331a2816dc24d8919d154ee5e9
wrrouman <106677200+wrrouman@users.noreply.github.com>
2022-12-04 13:56:07 -0500

    Upload page responsive design

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -10,35 +10,52 @@ const Upload = () => {
 
       <div className="upload">
         <h2 className="upload__title">Upload Video</h2>
-        <div>
-          <div className="upload__section--thumbnail">VIDEO THUMBNAIL</div>
-          <img
-            src={UploadImage}
-            className="upload__thumbnail--image"
-            alt="bike"
-          ></img>
+        <div className="test">
+          {/* <div className="upload__containerZ"> */}
+            <div className="upload__container1">
+              <div className="upload__section--thumbnail">VIDEO THUMBNAIL</div>
+              <img
+                src={UploadImage}
+                className="upload__thumbnail--image"
+                alt="bike"
+              ></img>
+            </div>
+          {/* </div> */}
+          <div className="test2">
+          <form id="publish" className="upload__section--comments">
+            <div className="upload__section--containerA">
+              <label className="upload__label--title">Title Your Video</label>
+              <input
+                id="enter__video--title"
+                for="video title"
+                placeholder="Add a title to your video"
+              ></input>
+            </div>
+            <div className="upload__section--containerB">
+              <label className="upload__label--title">
+                Add a Video Description
+              </label>
+              <textarea
+                id="upload__description"
+                placeholder="Add a description to your video"
+              ></textarea>
+            </div>
+          </form>
+          </div>
         </div>
 
-        <form id='publish' className="upload__section--comments">
-          <div className="upload__section--containerA">
-            <label className="upload__label--title">Title Your Video</label>
-            <input id='enter__video--title' for='video title' placeholder='Add a title to your video'></input>
-          </div>
-          <div className="upload__section--containerB">
-            <label className="upload__label--title">Add a Video Description</label>
-            <textarea  id="upload__description" placeholder='Add a description to your video'></textarea>
-          </div>
+        <div className="upload__section--containerC">
           <div className="upload__publish--button">
-            <button id='publish__button'>Publish</button>
-            <Link to="/"  style={{ textDecoration: 'none' }}>
-              <div className="upload__cancel--container">
-              <button id="cancel" >Cancel</button>
-              </div>
-            </Link>
+            <button id="publish__button">Publish</button>
           </div>
-        </form>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="upload__cancel--container">
+              <button id="cancel">Cancel</button>
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
 };
-export default Upload
+export default Upload;

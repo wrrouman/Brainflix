86b669399d3792eb3737c5b51b6b708095f79ca1
wrrouman <106677200+wrrouman@users.noreply.github.com>
2022-12-04 14:06:47 -0500

    Saas touchup

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -10,37 +10,36 @@ const Upload = () => {
 
       <div className="upload">
         <h2 className="upload__title">Upload Video</h2>
-        <div className="test">
-          {/* <div className="upload__containerZ"> */}
-            <div className="upload__container1">
-              <div className="upload__section--thumbnail">VIDEO THUMBNAIL</div>
-              <img
-                src={UploadImage}
-                className="upload__thumbnail--image"
-                alt="bike"
-              ></img>
-            </div>
-          {/* </div> */}
-          <div className="test2">
-          <form id="publish" className="upload__section--comments">
-            <div className="upload__section--containerA">
-              <label className="upload__label--title">Title Your Video</label>
-              <input
-                id="enter__video--title"
-                for="video title"
-                placeholder="Add a title to your video"
-              ></input>
-            </div>
-            <div className="upload__section--containerB">
-              <label className="upload__label--title">
-                Add a Video Description
-              </label>
-              <textarea
-                id="upload__description"
-                placeholder="Add a description to your video"
-              ></textarea>
-            </div>
-          </form>
+        <div className="upload__test-container">
+          <div className="upload__container1">
+            <div className="upload__section--thumbnail">VIDEO THUMBNAIL</div>
+            <img
+              src={UploadImage}
+              className="upload__thumbnail--image"
+              alt="bike"
+            ></img>
+          </div>
+
+          <div className="upload__test2">
+            <form id="publish" className="upload__section--comments">
+              <div className="upload__section--containerA">
+                <label className="upload__label--title">Title Your Video</label>
+                <input
+                  id="enter__video--title"
+                  for="video title"
+                  placeholder="Add a title to your video"
+                ></input>
+              </div>
+              <div className="upload__section--containerB">
+                <label className="upload__label--title">
+                  Add a Video Description
+                </label>
+                <textarea
+                  id="upload__description"
+                  placeholder="Add a description to your video"
+                ></textarea>
+              </div>
+            </form>
           </div>
         </div>
 
